1966446514164ff9fff9806b64cc7b92ae79a323
baznas <baznasptk@gmail.com>
2017-11-09 09:54:05 +0700

    next commit

---
+++ app/src/main/java/groucode/sikerja/activity/PenjelasanActivity.java
@@ -0,0 +1,8 @@
+package groucode.sikerja.activity;
+
+/**
+ * Created by User on 09/11/2017.
+ */
+
+public class PenjelasanActivity {
+}
---
+++ app/src/main/java/groucode/sikerja/app/AppConfig.java
@@ -0,0 +1,8 @@
+package groucode.sikerja.app;
+
+/**
+ * Created by User on 09/11/2017.
+ */
+
+public class AppConfig {
+}
